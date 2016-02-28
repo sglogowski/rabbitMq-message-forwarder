2a3f5f797bdf792324eea3e0fa4bf073e92230c1
Sebastian Głogowski <s.glogowski@outlook.com>
2016-02-28 20:37:32 +0100

    basic implementation.

---
+++ src/MessageForwarder/Arguments.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace MessageForwarder
+{
+    internal class Arguments
+    {
+        [Option("fromQueue", Required = true)]
+        public string FromQueue { get; set; }
+
+        [Option("toExchange", Required = true)]
+        public string ToExchange { get; set; }
+
+        [Option("host", Required = false, DefaultValue = "localhost")]
+        public string Host { get; set; }
+
+        [Option("port", Required = false, DefaultValue = 5672)]
+        public int Port { get; set; }
+
+        [Option("userName", Required = false, DefaultValue = "guest")]
+        public string UserName { get; set; }
+
+        [Option("password", Required = false, DefaultValue = "guest")]
+        public string Password { get; set; }
+
+        [Option("publisherRoutingKey", Required = false, DefaultValue = "")]
+        public string PublisherRoutingKey { get; set; }
+    }
+}
---
+++ src/MessageForwarder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using CommandLine;
+using EasyNetQ;
+using EasyNetQ.Topology;
+using NLog;
+
+namespace MessageForwarder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            var arguments = new Arguments();
+            if (!Parser.Default.ParseArguments(args, arguments))
+                return;
+
+            var connectionString = $"host={arguments.Host};port={arguments.Port};username={arguments.UserName};password={arguments.Password}";
+            var advancedBus = RabbitHutch.CreateBus(connectionString).Advanced;
+            var exchange = advancedBus.ExchangeDeclare(arguments.ToExchange, ExchangeType.Direct, true);
+            var queue = advancedBus.QueueDeclare(arguments.FromQueue, true);
+
+            advancedBus.Consume(queue, async (body, properties, info) =>
+            {
+                logger.Info(Encoding.UTF8.GetString(body));
+
+                await advancedBus.PublishAsync(exchange, arguments.PublisherRoutingKey, false, new MessageProperties(), body);
+            });
+        }
+    }
+}
